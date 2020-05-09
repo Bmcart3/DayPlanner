50eaa2a587959bb98ce216c590bf949b1c480f1c
bmcart3 <bmcart3@gmail.com>
2020-05-09 19:24:50 -0400

    adding clearer comments for code

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,11 @@ $(document).ready(function () {
     var currentDate = Date(Date.now());
     $("#currentDay").text(currentDate);
 
-
+    //This function will be checking the current time against the time blocks to determine if it is past present or future and coloring the boxes accordingly.
+    //new Date().getHours() gives the current military hour as a number.
+    //This loops once for each textarea element based on its description class.
+    //It compares value of the data attribute data-milTime against the current military hour.
+    //depending on the outcome, it adds or removes a class to show the correct color.
     function hourUpdater() {
         var currentHour = new Date().getHours();
         $(".description").each(function () {
@@ -19,14 +23,21 @@ $(document).ready(function () {
             }
         })
     };
-
+    //calls the hourUpdater function first.
+    //set on an interval to update once per minute.
     hourUpdater();
     setInterval(hourUpdater, 60000);
 
+
+    //click event on the save button
+    //creates variable chores which is equal to the value of the textarea (the user input) which was grabbed through the sibling of the savebtn. This becomes the value for localstorage.
+    //creates variable hour which is equal to the id value of the parent of the savebtn which is the div. This becomes the key for localstorage.
+    //Can then store the users input and associate it to a specific specific textarea.
     $(".saveBtn").on("click", function () {
         var chores = $(this).siblings(".description").val();
         var hour = $(this).parent().attr("id");
         localStorage.setItem(hour, chores);
+        console.log(hour)
         
     })
     
@@ -40,6 +51,8 @@ $(document).ready(function () {
     $("#hour-16 .description").val(localStorage.getItem("hour-16"));
     $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 })
+
+
 
 
 // # JavaScript
